56e42e7569970088956693bd7a046601f924002f
mzums <annsie035@gmail.com>
2024-08-31 22:05:18 +0200

    works

--- cliff_walking/main.py
+++ cliff_walking/main.py
@@ -1,17 +1,24 @@
 import gymnasium as gym
 import numpy as np
+import matplotlib.pyplot as plt
 import pickle
 
 def run(episodes, is_training=True, render=False):
     env = gym.make('CliffWalking-v0', render_mode='human' if render else None)
 
-    q = np.zeros((env.observation_space.n, env.action_space.n)) # 48 x 4 array
+    if(is_training):
+        q = np.zeros((env.observation_space.n, env.action_space.n)) # 64 x 4 array
+    else:
+        f = open('cliff_walking.pkl', 'rb')
+        q = pickle.load(f)
+        f.close()
 
     epsilon = 1
     epsilon_decay = 0.0001
     learning_rate = 0.9
     discount_factor = 0.9
     rng = np.random.default_rng()
+    rewards_per_episode = np.zeros(episodes)
 
     for i in range(episodes):
         state = env.reset()[0]
@@ -25,6 +32,7 @@ def run(episodes, is_training=True, render=False):
                 action = np.argmax(q[state, :])
             
             new_state,reward,terminated,truncated,_ = env.step(action)
+            rewards_per_episode[i] += reward
             if is_training:
                 q[state, action] = q[state, action] + learning_rate * (reward + discount_factor * np.max(q[new_state, :]) - q[state, action])
 
@@ -35,10 +43,20 @@ def run(episodes, is_training=True, render=False):
 
     env.close()
 
-    f = open("cliff_walking.pkl", "wb")
-    pickle.dump(q, f)
-    f.close()
+    sum_rewards = np.zeros(episodes)
+    for ep in range(episodes):
+        sum_rewards[ep] = np.mean(rewards_per_episode[max(0, ep-100):(ep+1)])
+
+    sum_rewards = sum_rewards[10:]
+    plt.plot(sum_rewards)
+    plt.savefig('cliff_walking.png')
+
+    if is_training:
+        f = open("cliff_walking.pkl", "wb")
+        pickle.dump(q, f)
+        f.close()
 
 
 if __name__ == '__main__':
-    run(1000)
+    #run(1000)
+    run(10, is_training=False, render=True)
